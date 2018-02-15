704c0796a94b87073b23017d17ec2460db33dc01
prafful13 <prafful.mehrotra@gmail.com>
2018-02-15 15:41:08 +0530

    fix #4

--- index.js
+++ index.js
@@ -42,9 +42,10 @@ require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+
   app.use(express.static(path.resolve(__dirname, "client", "build")));
 
-  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
